42a89a2caaae0435b85ffd0b20a74717d0a1e866
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2022-06-13 15:47:59 -0400

    Update

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.set("port", process.env.PORT || 5000);
+const mongoose = require("mongoose");
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+//require('dotenv').config();
+//const url=process.env.MONGODB_URI;
+//const client=new MongoClient(url);
+//client.connect();
+
+mongoose.connect(
+  "mongodb+srv://ash:o3LGLnokqxyyAdd3@cluster0.1lp73.mongodb.net/User?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Database connected");
+});
+
+const userSignUp = require("./backend/routes/signup");
+const userLogin = require("./backend/routes/login");
+const userTravel = require("./backend/routes/travelPage");
+const userAddTrip = require("./backend/routes/addtrip");
+const userDeleteTrip = require("./backend/routes/deletetrip");
+const userSingleTrip = require("./backend/routes/singleTrip");
+const userSearchTrip = require("./backend/routes/searchTrip");
+const verifyAccount = require("./backend/routes/verifyEmail");
+const userUpdateTrip = require("./backend/routes/updateTrip");
+
+app.use("/signup", userSignUp);
+app.use("/login", userLogin);
+app.use("/travel", userTravel);
+app.use("/addTrip", userAddTrip);
+app.use("/deleteTrip", userDeleteTrip);
+app.use("/singleTrip", userSingleTrip);
+app.use("/searchTrip", userSearchTrip);
+app.use("/verify", verifyAccount);
+app.use("/updateTrip", userUpdateTrip);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
+
+// For Heroku deployment
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+process.on("SIGINT", function () {
+  console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
+  // some other closing procedures go here
+  process.exit(1);
+});
